2d85050d65ab1318a9bac99d5bc34979c2d44cc3
Andres Ccopa <Andres.ccopa@gestionysistemas.com>
2020-04-03 19:58:24 -0500

    rx

--- app/src/main/java/com/test/rxandroid/MainActivity.java
+++ app/src/main/java/com/test/rxandroid/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,8 +15,12 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -52,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
         List<Observable<?>> requests = new ArrayList<>();
 
-        String token = "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjIzNTJjM2Q2NDIwYTI0YmFiZTRmMTU1ZDRiODI1ZGY5IiwidHlwIjoiSldUIn0.eyJuYmYiOjE1ODIxNTYxNzYsImV4cCI6MTU4MjIwNjE3NiwiaXNzIjoiaHR0cDovL29uYm9hcmRpbmctbGItZGV2LTE3Njk5NDkyMzUudXMtZWFzdC0xLmVsYi5hbWF6b25hd3MuY29tL2lkZW50aXR5IiwiYXVkIjpbImh0dHA6Ly9vbmJvYXJkaW5nLWxiLWRldi0xNzY5OTQ5MjM1LnVzLWVhc3QtMS5lbGIuYW1hem9uYXdzLmNvbS9pZGVudGl0eS9yZXNvdXJjZXMiLCJvbmJvYXJkaW5nYXBpIl0sImNsaWVudF9pZCI6IndlYiIsInN1YiI6IiIsImF1dGhfdGltZSI6MTU4MjE1NjE3NiwiaWRwIjoibG9jYWwiLCJ1c2VySWQiOiJneXMiLCJ1c2VyTmFtZSI6Ikdlc3Rpb24geSBTaXN0ZW1hcyIsInVzZXIiOiJHZXN0aW9uIHkgU2lzdGVtYXMiLCJjb3JyZW8iOiJneXNAZ2VzdGlvbnlzaXN0ZW1hcy5jb20iLCJjaGFuZ2VQYXNzd29yZCI6IjAiLCJleHBpcmVkUGFzc3dvcmQiOiIwIiwicm9sSWQiOiJSMDAwMiIsInJvbE5hbWUiOiJHZXN0b3IiLCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwib25ib2FyZGluZ2FwaSJdLCJhbXIiOlsibXlzcWxkYiJdfQ.cIRqZsFpatwJ_w4UsZL4ebLpPaFTTixoYYzviS8x_bXkIMWwaZIWgkoDpjMRWcXwxW6-1GsIOuuxItxMBTCj88plXKeC3KB27fV6_8YPZZf70W0W40YTwfngd7Rr_sw0wpVDvxt3y6kjb-kV6fHfcNGmAs6LeCIqMBtVH9-yQKjBqW__Dczx5wsmlY-rNECvmNVOiSz5jS_JJlzIvgxNoJpVBArvJnZo955KEfOhYcY_4A8qNR7_FRvKwUGgcNgrnNn-7uTmyqw20fYAEp_RCcqg8HUNNk3wh7vMCrCqJvTtHhUYuMrAQcjRy395fWP9MEYS6hCDx3OWg36l1-uRuA";
+        String token = "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjVBQTFBNDMzRDQxNjI5NzI4NTk1QUEzQzAyMEVDRDJCRTg3Mzc1NDYiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJXcUdrTTlRV0tYS0ZsYW84QWc3TkstaHpkVVkifQ.eyJuYmYiOjE1ODU5NTgzODcsImV4cCI6MTU4NjAwODM4NywiaXNzIjoiaHR0cDovL29uYm9hcmRpbmctbGItZGV2LTE3Njk5NDkyMzUudXMtZWFzdC0xLmVsYi5hbWF6b25hd3MuY29tL2lkZW50aXR5IiwiYXVkIjpbImh0dHA6Ly9vbmJvYXJkaW5nLWxiLWRldi0xNzY5OTQ5MjM1LnVzLWVhc3QtMS5lbGIuYW1hem9uYXdzLmNvbS9pZGVudGl0eS9yZXNvdXJjZXMiLCJjYXBwdGFhcGkiLCJvbmJvYXJkaW5nYXBpIl0sImNsaWVudF9pZCI6IndlYiIsInN1YiI6IiIsImF1dGhfdGltZSI6MTU4NTk1ODM4NywiaWRwIjoibG9jYWwiLCJ1c2VySWQiOiJ1c3VwZXJ2aXNvciIsInVzZXJOYW1lIjoiR2VzdGlvbiB5IFNpc3RlbWFzIiwidXNlciI6Ikdlc3Rpb24geSBTaXN0ZW1hcyIsImNvcnJlbyI6Im1pbGFncm9zQGdlc3Rpb255c2lzdGVtYXMuY29tIiwiY2hhbmdlUGFzc3dvcmQiOiIwIiwiZXhwaXJlZFBhc3N3b3JkIjoiMCIsInJvbElkIjoiUjAwMDMiLCJyb2xOYW1lIjoiU3VwZXJ2aXNvciIsInNjb3BlIjpbIm9wZW5pZCIsInByb2ZpbGUiLCJjYXBwdGFhcGkiLCJvbmJvYXJkaW5nYXBpIl0sImFtciI6WyJteXNxbGRiIl19.fuHwejqu9Oq5UJiCaoDVb-TFUGvhN4QT0FwOCHbizQNz0X-Tq0QmzbwNT1P1EQWy1AcpAGIhp0o72ZHUySe8Z-tAoMUKtZaaNqGliRzBHSesnL0I_qaQhC0__q5ZuQpwOM0XrA9hn71wFCU-nczAcBm6Kc-EQERJh7xqrAJYOmSRKlgqSll-hMtjyrxO404qCpUtnV7sAwDTTvfQF2F-u0gYgSz0qx9KDr_MAzNUHLew7d-MdHXl02-E8JIKsCHyDe1VWwb9ShZBIvE_pAxgIOd02UfErc7nk8nBJvsysOPR48jolBaU7R3yDMZwtEgU6TbGX_o3IAaslq8ZPxz4ag";
 
         requests.add(getRequest.getParameters(token, Constants.PARAMETER_DEPARTMENT));
         requests.add(getRequest.getParameters(token, Constants.PARAMETER_PROVINCE));
@@ -67,23 +72,30 @@ public class MainActivity extends AppCompatActivity {
                         // do something with those results and emit new event
                         return new Object();
                     }
-                }).subscribe(
-                // Will be triggered if all requests will end successfully (4xx and 5xx also are successful requests too)
-                new Consumer<Object>() {
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<Object>() {
                     @Override
-                    public void accept(Object o) throws Exception {
-                        //Do something on successful completion of all requests
+                    public void onSubscribe(Disposable d) {
+                        //Toast.makeText(getApplicationContext(),"onSubscribe", Toast.LENGTH_SHORT).show();
                     }
-                },
 
-                // Will be triggered if any error during requests will happen
-                new Consumer<Throwable>() {
                     @Override
-                    public void accept(Throwable e) throws Exception {
-                        //Do something on error completion of requests
+                    public void onNext(Object o) {
+                        Toast.makeText(getApplicationContext(),"onNext", Toast.LENGTH_SHORT).show();
                     }
-                }
-        );
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(getApplicationContext(),"onError", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Toast.makeText(getApplicationContext(),"onComplete", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     @OnClick(R.id.btn_call_service)
